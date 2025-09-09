78182983e3478d674e13d03b859f8e79931c70c3
singhakash006 <singhakash6203@gmail.com>
2025-09-09 18:25:31 +0530

    assignment file added

---
+++ conditional.py
@@ -0,0 +1,44 @@
+# x = int(input("Enetr a number: "))
+
+# if(x > 0):
+#     x+=1
+#     print(x)
+# else: 
+#     print("Number is Negative.")
+
+# print("Write a program to print a person is eligible to vote, if he is not eligible to vote the dispplay how many years left to eligible")
+
+# age = int(input("Enter the age of person: "))
+
+# if(age > 18):
+#     print("You are eligible to vote.")
+# else:
+#     print(f"You will become eligible after {18-age} years.")
+
+
+# print("Write a program to determine the character entered by a user")
+
+# char = input("Press any key : ")
+# if(char.isalpha()):
+#     print("It is a character.")
+# elif(char.isdigit()):
+#     print("It is a digit.")
+# elif(char.isspace()):
+#     print("It is a whiteb space.")
+# else:
+#     print("No valid character.")
+
+# print("Write a program to find greater between two numbers ")
+
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+
+# if(a > b):
+#     print(f"{a} is larger.")
+# else:
+#     print(f"{b} is larger.")
+
+print("if a person enter character in lowercase then convert to uppercase else convert upper to lower. ")
+s = input("Enter a character.")
+
+
